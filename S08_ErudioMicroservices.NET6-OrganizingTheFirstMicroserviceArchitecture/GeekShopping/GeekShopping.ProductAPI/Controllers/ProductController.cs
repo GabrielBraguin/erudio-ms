744d6cbece9459d23e138a15ac2ec31abaca0712
Gabriel Braguin <gabriel.braguin@hotmail.com.br>
2022-12-29 19:41:55 -0300

    crud added

---
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicroserviceArchitecture/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using GeekShopping.ProductAPI.Data.DTOs;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDTO>> Create(ProductDTO dTO)
+        {
+            if (dTO == null) return BadRequest();
+            await _repository.Create(dTO);
+            return CreatedAtAction(nameof(Create), new { id = dTO.Id }, dTO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(ProductDTO dTO)
+        {
+            if (dTO == null) return BadRequest(); 
+            await _repository.Update(dTO);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return NoContent();
+        }
+    }
+}
--- S08_ErudioMicroservices.NET6-OrganizingTheFirstMicroserviceArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ S08_ErudioMicroservices.NET6-OrganizingTheFirstMicroserviceArchitecture/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,12 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
     connection,
     new MySqlServerVersion(new Version(8, 0, 30)))
 );
+
+IMapper mapper = MappingConfiguration.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 // Add services to the container.
 
